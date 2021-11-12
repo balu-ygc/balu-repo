467756da54a2141a8348051b233aad2af215a67b
balu-ygc <87455532+balu-ygc@users.noreply.github.com>
2021-11-12 10:27:10 +0530

    Update javaworld.java

--- javaworld.java
+++ javaworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch21 team");
-      System.out.println("Hello Batch21 with additional information");
+      System.out.println("Hello Batch21 Bala Avik with additional information");
     }  
 } 
